66b32796c2f154f5654ddb567513fe3d9fcc5a1c
GJ Tiquia <gershom.tiquia@gmail.com>
2025-05-23 10:57:14 -0400

    added aliases to directories

--- OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 echo "loading profile.ps1..."
 
-function so { . $PROFILE }
+function obsidian { cd D:\Documents\SelfProjects\obsidian-vault }
+function 9cat { cd D:\Documents\UnityProjects\9cat-2d-2 }
 
 # dotfile management
 $DOTFILES_HOME = $HOME
