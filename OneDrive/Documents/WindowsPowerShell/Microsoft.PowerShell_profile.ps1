629c715283a32695b5b8eb2d536b8ccc2b254027
GJ Tiquia <gershom.tiquia@gmail.com>
2025-06-04 12:36:29 -0400

    added aliases and yazi shell wrapper

--- OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,8 @@ function vim { nvim $args }
 function lg { lazygit $args }
 function q { exit $args }
 function c { clear $args }
+function ifconfig { Get-NetIPConfiguration }
+function sshd { Start-Service sshd }
 
 # dotfiles location aliases
 function nvimrc { cd ~\AppData\Local\nvim }
@@ -25,3 +27,15 @@ function dotfiles { git --git-dir=$DOTFILES_HOME/$DOTFILES_GIT_DIR/ --work-tree=
 Set-PSFzfOption -EnableAliasFuzzyHistory
 # fcd = alias for fzf cd
 function fcd { Invoke-FuzzySetLocation }
+
+# yazi - y shell wrapper
+# https://yazi-rs.github.io/docs/quick-start#shell-wrapper
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
